e4cca5157ab2f48f3be1cf2ec51e97236140bddc
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-11 13:34:27 +0530

    Added: Check absent or present

--- src/com/bl/employeewage/EmpWageBuilder.java
+++ src/com/bl/employeewage/EmpWageBuilder.java
@@ -2,7 +2,30 @@ package com.bl.employeewage;
 
 public class EmpWageBuilder
 {
+    public static void employeeAttendace() {
+
+        int IS_PRESENT = 1;
+
+        double empCheck = Math.floor((Math.random() * 10) % 2);  //using random method for random no
+
+        /*
+         * check employee is present or absent
+         */
+
+        if (empCheck == IS_PRESENT) {
+
+            System.out.println("Employee is Present");
+
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Start employee wage using OOP's");
+
+        EmpWageBuilder.employeeAttendace(); //main method
     }
 }
