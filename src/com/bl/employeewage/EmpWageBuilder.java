64515f5e02649b2fbc11156a69fac0ffaa26cb3a
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-11 13:39:07 +0530

    for calcualte Part Time Employee Wage.

--- src/com/bl/employeewage/EmpWageBuilder.java
+++ src/com/bl/employeewage/EmpWageBuilder.java
@@ -4,35 +4,38 @@ public class EmpWageBuilder
 {
 
 
-        public static final int IS_PRESENT = 1;
-        public static final int SALARY_PER_HR = 20;
+    public static final int IS_PRESENT = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int SALARY_PER_HR = 20;
 
-        public static void employeeAttendace() {
+    public static void employeeAttendace() {
 
-            double empCheck = Math.floor((Math.random() * 10) % 2);
+        double empCheck = Math.floor((Math.random() * 10) % 2);
 
-            int Working_Hr = 0;
+        int Working_Hr = 0;
 
-        /*
-        checking condition
-         */
-
-            if (empCheck == IS_PRESENT) {
-
-                Working_Hr = 8;
-
-            }
-
-            double Salary = Working_Hr * SALARY_PER_HR;
-
-            System.out.println("Employee Salary is = " + Salary + "  Doller");
+        if (empCheck == IS_PRESENT) {
+            Working_Hr = 8;
 
         }
 
+        else if (empCheck == IS_PART_TIME) {
+            Working_Hr = 4;
 
-        public static void main(String[] args)
+        }
+        else
         {
-            EmpWageBuilder.employeeAttendace();  //main method
+            Working_Hr = 0;
         }
 
+        double Salary = Working_Hr * SALARY_PER_HR;
+
+        System.out.println("Emplyee Salary is = " + Salary + "  Doller");
+
+    }
+
+    public static void main(String[] args) {
+
+        EmpWageBuilder.employeeAttendace();     //main method
+    }
     }
