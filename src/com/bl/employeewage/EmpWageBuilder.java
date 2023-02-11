99b4c2fbde24b73fb27e7502f2d662ec82ac993a
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-11 13:30:32 +0530

    Start program using oops

---
+++ src/com/bl/employeewage/EmpWageBuilder.java
@@ -0,0 +1,8 @@
+package com.bl.employeewage;
+
+public class EmpWageBuilder
+{
+    public static void main(String[] args) {
+        System.out.println("Start employee wage using OOP's");
+    }
+}
